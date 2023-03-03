17f63e52fca0c89ef5cebfdd4672aa14a19aa99f
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-03 16:35:05 +0100

    03.03

--- dist/app.js
+++ dist/app.js
@@ -6,4 +6,28 @@
 // Wyrzucić śmieci
 // Pójść na siłownię
 // Nakarmić koty
-const tasks = ['Wyrzucić śmieci', 'Pójść na siłownię', 'Nakarmić koty'];
+const taskNameInputElement = document.querySelector('#name');
+const addButtonElement = document.querySelector('button');
+const tasksContainerElement = document.querySelector('.tasks');
+const tasks = [
+    { name: 'Wyrzucić śmieci', done: false },
+    { name: 'Pójść na siłownię', done: false },
+    { name: 'Nakarmić koty', done: false }
+];
+const render = () => {
+    tasksContainerElement.innerHTML = '';
+    tasks.forEach(task => {
+        const taskElement = document.createElement('li');
+        taskElement.innerText = task.name;
+        tasksContainerElement.appendChild(taskElement);
+    });
+};
+const addTask = (taskName) => {
+    tasks.push({ name: taskName, done: false });
+};
+addButtonElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    addTask(taskNameInputElement.value);
+    render();
+});
+render();
--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,36 @@
 // Pójść na siłownię
 // Nakarmić koty
 
-const tasks = ['Wyrzucić śmieci', 'Pójść na siłownię', 'Nakarmić koty'];
+const taskNameInputElement: HTMLInputElement = document.querySelector('#name');
+const addButtonElement: HTMLButtonElement = document.querySelector('button');
+const tasksContainerElement: HTMLElement = document.querySelector('.tasks'); 
+
+const tasks: {
+    name: string;
+    done: boolean;
+}[] = [
+	{name: 'Wyrzucić śmieci', done: false},
+	{name: 'Pójść na siłownię', done: false},
+	{name: 'Nakarmić koty', done: false}
+];
+
+const render = () => {
+	tasksContainerElement.innerHTML = '';
+	tasks.forEach(task => {
+		const taskElement: HTMLElement = document.createElement('li');
+		taskElement.innerText = task.name;
+		tasksContainerElement.appendChild(taskElement);
+	})
+}
+
+const addTask = (taskName: string) => {
+	tasks.push({name: taskName, done: false});
+}
+
+addButtonElement.addEventListener('click', (e: Event) => {
+	e.preventDefault();
+	addTask(taskNameInputElement.value);
+	render();
+})
+
+render();
